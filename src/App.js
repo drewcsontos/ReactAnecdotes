1f09f6e72051bd5009308328d964c1b858a6959f
drewcsontos <drew@csontos.net>
2021-03-19 13:04:56 -0400

    commit 3

--- src/App.js
+++ src/App.js
@@ -1,67 +1,49 @@
-import React from "react";
-
-const Course = (props) => {
-  return <h1>{props.course.name}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part
-        name={props.course.parts[0].name}
-        exercises={props.course.parts[0].exercises}
-      />
-      <Part
-        name={props.course.parts[1].name}
-        exercises={props.course.parts[1].exercises}
-      />
-      <Part
-        name={props.course.parts[2].name}
-        exercises={props.course.parts[2].exercises}
-      />
-    </div>
-  );
-};
-const Total = (props) => {
-  let sum = 0;
-  props.course.parts.forEach((element) => {
-    sum += element.exercises;
-  });
-  return <p>Number of exercises {sum}</p>;
-};
+import React, { useState } from "react";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  ];
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(
+    [...anecdotes].fill(0, 0, anecdotes.length)
+  );
+  const [mostVotes, setMostVotes] = useState([anecdotes[0], 0]);
   return (
     <div>
-      <Course course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button
+        onClick={() => {
+          let votesCopy = [...votes];
+          votesCopy[selected] += 1;
+          let max = Math.max.apply(Math, votesCopy);
+          setMostVotes([anecdotes[votesCopy.indexOf(max)], max]);
+          setVotes(votesCopy);
+        }}
+      >
+        vote
+      </button>
+      <button
+        onClick={() => setSelected(getRandomInt(0, anecdotes.length - 1))}
+      >
+        next anecdote
+      </button>
+      <h1>Anecdote with most votes</h1>
+      <p>{mostVotes[0]}</p>
+      <p>has {mostVotes[1]} votes</p>
     </div>
   );
 };
